6e2d579dd54b7d29832d91989cf7465f6843c297
Akshit Yadav <akshit.yadav@kognitokube.com>
2025-12-11 13:36:01 +0530

    init

---
+++ src/main/java/com/learn_jpa/learn_jpa/LearnJpaApplication.java
@@ -0,0 +1,13 @@
+package com.learn_jpa.learn_jpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnJpaApplication.class, args);
+	}
+
+}
